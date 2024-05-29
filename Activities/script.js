50a19ca25ec6e9971981c21e0481690a70addc56
Ethan FARGIER <fargier.ethan@gmail.com>
2024-05-29 10:48:53 +0100

    Update activities

---
+++ Activities/script.js
@@ -0,0 +1,10 @@
+const slide = ["foret-peuplier.jpg", "paysage-montagne.jpg", "chemin-automne.jpg", "prairie-alpes.jpg"];
+let numero = 0;
+
+function ChangeSlide(sens) {
+    numero = numero + sens;
+    if (numero < 0)
+        numero = slide.length - 1;
+    if (numero > slide.length - 1)
+        numero = 0;
+    document.getElementById("slide").src = slide[numero];
